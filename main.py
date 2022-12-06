1f31411409efa57a1212e3c66f2f86a0d13701b6
Sergey Kievskiy <knhel7@mail.ru>
2022-12-06 13:47:01 +0300

    vk_bot edited, start button added, config created

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import vk_tools
+#
+
 from vk_tools.vk_bot import VkBot
-import psycopg2
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from db_tools.models import create_tables, drop_tables, Users, Candidate, MarkList
+
+# import psycopg2
+# import sqlalchemy
+# from sqlalchemy.orm import sessionmaker
+# from db_tools.models import create_tables, drop_tables, Users, Candidate, MarkList
 
 # проверил через собственную БД - ошибок при запуске функций создания и удаления таблиц не было.
 
-user = 'postgres'
-password = 'введите свой пароль'
-host = 'localhost'
-base_name = 'vkinder_db'  # название можно сменить.
-DSN = f'postgresql://{user}:{password}@{host}:5432/{base_name}'
-engine = sqlalchemy.create_engine(DSN)
+# user = 'postgres'
+# password = 'введите свой пароль'
+# host = 'localhost'
+# base_name = 'vkinder_db'  # название можно сменить.
+# DSN = f'postgresql://{user}:{password}@{host}:5432/{base_name}'
+# engine = sqlalchemy.create_engine(DSN)
 
 if __name__ == '__main__':
     bot = VkBot()
