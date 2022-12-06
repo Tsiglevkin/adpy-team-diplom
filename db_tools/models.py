1cb801d531313441bf87789c0317e527dee6125b
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2022-12-06 23:52:04 +0500

    finished function from models. tests have done in main.

--- db_tools/models.py
+++ db_tools/models.py
@@ -2,7 +2,6 @@ import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 from tqdm import tqdm
 
-
 Base = declarative_base()
 
 
@@ -55,22 +54,31 @@ def drop_tables(engine):
     Base.metadata.drop_all(engine)
 
 
-def show_all_candidates_id(some_session, some_user_vk_id: int) -> list:
-    """Функция возвращает список кандидатов для конкретного пользователя"""
-    return some_session.query(Candidate.vk_id).join(MarkList.candidate).join(MarkList.user).filter(
-            Users.vk_id == some_user_vk_id)
+def show_all_candidates_id(some_session, user_vk_id: int) -> list:
+    """Функция возвращает список кандидатов без отметок для конкретного пользователя"""
+    res_list = []
+    for c in some_session.query(Candidate.vk_id).join(MarkList.candidate).join(MarkList.user).filter(
+            Users.vk_id == user_vk_id, MarkList.like == 'False', MarkList.dislike == 'False').all():
+        res_list.append(c[0])
+    return res_list
 
 
-def show_all_like_candidates(some_session, some_user_vk_id: int) -> list:  # метод не проверен, сработает ли 'and'?
+def show_all_like_candidates(some_session, user_vk_id: int) -> list:  # метод не проверен, сработает ли 'and'?
     """Функция выводит список кандидатов, отмеченных лайком"""
-    return some_session.query(Candidate.vk_id).join(MarkList.candidate).join(MarkList.user).filter(
-            MarkList.like is True and Users.vk_id == some_user_vk_id)
+    res_list = []
+    for c in some_session.query(Candidate.vk_id).join(MarkList.candidate).join(MarkList.user).filter(
+            MarkList.like == 'True', Users.vk_id == user_vk_id).all():
+        res_list.append(c[0])
+    return res_list
 
 
-def show_black_list(some_session, some_user_vk_id: int) -> list:
+def show_black_list(some_session, user_vk_id: int) -> list:
     """Функция выводит список кандидатов, отмеченных дизлайком"""
-    return some_session.query(Candidate).join(MarkList.candidate).join(MarkList.user).filter(
-            MarkList.dislike is True and Users.vk_id == some_user_vk_id)
+    res_list = []
+    for c in some_session.query(Candidate.vk_id).join(MarkList.candidate).join(MarkList.user).filter(
+            MarkList.dislike == 'True', Users.vk_id == user_vk_id).all():
+        res_list.append(c[0])
+    return res_list
 
 
 def add_id_to_users(some_session, user_vk_id_list: list):
@@ -79,41 +87,63 @@ def add_id_to_users(some_session, user_vk_id_list: list):
         new_user = Users(vk_id=id_)
         some_session.add(new_user)
         some_session.commit()
+    return 'ID пользователей добавлены'
 
 
-def add_id_to_candidates(some_session, user_vk_id: int, candidate_vk_id: int) -> str:
-    """Функция добавляет пользователя в таблицу Candidate и в таблицу MarkList.
-    Требуются vk_id от пользователя и кандидата"""
-    new_candidate = Candidate(vk_id=candidate_vk_id)
-    some_session.add(new_candidate)
-    some_session.commit()
+def add_id_to_candidates(some_session, candidate_vk_id_list: list):
+    """Функция добавляет пользователя в таблицу Candidate"""
+    for id_ in tqdm(candidate_vk_id_list, desc='Добавляем id в БД'):
+        new_candidate = Candidate(vk_id=id_)
+        some_session.add(new_candidate)
+        some_session.commit()
+    return 'ID кандидатов добавлены'
 
-    user_id_for_mark = some_session.query(Users).filter(Users.vk_id == user_vk_id)
-    candidate_id_for_mark = some_session.query(Candidate).filter(Candidate.vk_id == candidate_vk_id)
 
-    new_mark_candidate = MarkList(user_id=user_id_for_mark, candidate_id=candidate_id_for_mark, like=False,
-                                  dislike=False)
-    some_session.add(new_mark_candidate)
-    some_session.commit()
-    return f'Пользователь с id {user_vk_id} добавлен в таблицу Candidate'
+def add_id_to_marklist(some_session, user_vk_id: int, candidate_vk_id: int) -> str:
+    """Функция добавляет пользователя в таблицу MarkList. Требуются vk_id от пользователя и кандидата"""
+    if user_vk_id != candidate_vk_id:
+        user_id = some_session.query(Users.user_id).filter(Users.vk_id == user_vk_id).scalar()
+        candidate_id = some_session.query(Candidate.candidate_id).filter(Candidate.vk_id == candidate_vk_id).scalar()
+
+        new_mark_candidate = MarkList(user_id=user_id, candidate_id=candidate_id, like=False, dislike=False)
+        some_session.add(new_mark_candidate)
+        some_session.commit()
+        return f'Для пользователя с ID {user_vk_id} добавлен кандидат с ID {candidate_vk_id}.'
+    else:
+        return 'пользователь и кандидат - один человек, смените пожалуйста кандидата.'
 
 
 def get_like(some_session, user_vk_id: int, candidate_vk_id: int) -> str:
     """Функция позволяет поставить like кандидату. Требуются vk_id от пользователя и кандидата"""
-    user_id = some_session.query(Users).filter(Users.vk_id == user_vk_id)
-    candidate_id = some_session.query(Candidate).filter(Candidate.vk_id == candidate_vk_id)
-    marked_candidate = MarkList(user_id=user_id, candidate_id=candidate_id, like=True, dislike=False)
-    some_session.add(marked_candidate)
+    user_id = some_session.query(Users.user_id).filter(Users.vk_id == user_vk_id).scalar()
+    candidate_id = some_session.query(Candidate.candidate_id).filter(Candidate.vk_id == candidate_vk_id).scalar()
+
+    some_session.query(MarkList).filter(MarkList.user_id == user_id, MarkList.candidate_id == candidate_id).\
+        update({'like': True, 'dislike': False})
     some_session.commit()
-    return f'Кандидату {candidate_id} поставлен like.'
+
+    return f'Кандидату {candidate_vk_id} поставлен like.'
 
 
 def get_dislike(some_session, user_vk_id: int, candidate_vk_id: int) -> str:
     """Функция позволяет поставить dislike кандидату. Требуются vk_id от пользователя и кандидата"""
-    user_id = some_session.query(Users).filter(Users.vk_id == user_vk_id)
-    candidate_id = some_session.query(Candidate).filter(Candidate.vk_id == candidate_vk_id)
-    marked_candidate = MarkList(user_id=user_id, candidate_id=candidate_id, like=False, dislike=True)
-    some_session.add(marked_candidate)
+    user_id = some_session.query(Users.user_id).filter(Users.vk_id == user_vk_id).scalar()
+    candidate_id = some_session.query(Candidate.candidate_id).filter(Candidate.vk_id == candidate_vk_id).scalar()
+
+    some_session.query(MarkList).filter(MarkList.user_id == user_id, MarkList.candidate_id == candidate_id). \
+        update({'like': False, 'dislike': True})
     some_session.commit()
-    return f'Кандидату {candidate_id} поставлен dislike.'
+    return f'Кандидату {candidate_vk_id} поставлен dislike.'
+
+
+def delete_mark(some_session, user_vk_id: int, candidate_vk_id: int) -> str:
+    """Функция позволяет убрать отметку like или dislike кандидату. Требуются vk_id от пользователя и кандидата"""
+    user_id = some_session.query(Users.user_id).filter(Users.vk_id == user_vk_id).scalar()
+    candidate_id = some_session.query(Candidate.candidate_id).filter(Candidate.vk_id == candidate_vk_id).scalar()
+
+    some_session.query(MarkList).filter(MarkList.user_id == user_id, MarkList.candidate_id == candidate_id). \
+        update({'like': False, 'dislike': False})
+    some_session.commit()
+    return f'Кандидату {candidate_vk_id} убрана оценка.'
+
 
